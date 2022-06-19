b8343cf4a56491b993c45a92cc5308f4e5a91df0
kienphan301 <kienphan30103@gmail.com>
2022-06-19 09:17:14 +0700

    ok

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import cv2
 import time
 import numpy as np
-import RPi.GPIO as GPIO
+"""import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)"""
 
 cars_classifier=cv2.CascadeClassifier('D:/Temp/Car Detection/accuratelyModel.xml')
 camera=cv2.VideoCapture(0)
 
-f_width = 1280
-f_height = 720
+f_width = 640
+f_height = 480
 
-offset2 = 150
-my_min = offset2
-my_max = f_width - offset2
-offset = 400
+offset = 180
 velocity = 0
 road_length=15
 start_time = 0
@@ -33,8 +30,8 @@ while(True):
     img = cv2.resize(img, (f_width, f_height))
     height,width=img.shape[0:2]
     
-    #cv2.line(img,(width-offset,0),(width-offset,height),(0,255,255),2)
-    #cv2.line(img,(offset,0),(offset,height),(255,0,0),2)
+    cv2.line(img,(width-offset,0),(width-offset,height),(0,255,255),2)
+    cv2.line(img,(offset,0),(offset,height),(255,0,0),2)
     
     blur=cv2.blur(img,(3,3))
     gray=cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
@@ -73,18 +70,20 @@ while(True):
             #velocity=velocity*0.036 #for km/h
             print(velocity)
             flag = 0
-            if (velocity > 10):
-                GPIO.output(7, True)
-                cv2.putText(img,"Van toc cua xe: "+str(velocity)+"cm/s - Qua toc do",(30,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
-            else:
-                GPIO.output(7, False)
-                cv2.putText(img,"Van toc cua xe: "+str(velocity)+"cm/s",(30,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+    if (velocity > 50):
+        #GPIO.output(7, True)
+        cv2.putText(img,"Van toc cua xe: "+str(velocity)+"cm/s",(30,40),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+    else:
+        #GPIO.output(7, False)
+        cv2.putText(img,"Van toc cua xe: "+str(velocity)+"cm/s",(30,40),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
                 
         
     cv2.imshow('LIVE',img)
     key=cv2.waitKey(15)
-    if key==27:
+    if key == 27:
         break
+    '''elif key == ord(x):
+        cv2.putText(img,"Van toc cua xe: 0 cm/s",(30,60),cv2.FONT_HERSHEY_COMPLEX,1.5,(0,255,0),3)'''
 
 cv2.destroyAllWindows()
 camera.release()
